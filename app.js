6e11219cd99bfe832da67644d5c0fe4a81537204
Artem <renerroll@gmail.com>
2025-04-26 00:02:50 +0300

    *repair

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
 
@@ -11,16 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/auth", authRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.use((error, req, res, next) => {
-  console.error(error);
-  res
-    .status(error.status || 500)
-    .json({ message: error.message || "Internal Server Error" });
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 app.listen(3000, () => {
